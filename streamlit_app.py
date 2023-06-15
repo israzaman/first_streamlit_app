844ec048a0fd6e7ae5dbe7d6de841e85009bb627
israzaman <39382093+israzaman@users.noreply.github.com>
2023-06-15 10:22:48 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
@@ -13,7 +13,7 @@ streamlit.text('🐔 Hard-Boiled Free-RangeEgg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
